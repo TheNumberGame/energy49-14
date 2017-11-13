dc043239c2da9aa3e977dce08db57e030b853866
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-11-13 10:59:54 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('Home.html')
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
