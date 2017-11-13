321f0bb0c769c16597172873341bae757fa19090
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-11-13 09:52:54 -0800

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,11 @@
+from flask import Flask, url_for, render_template, request, Markup, flash
+import os, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
