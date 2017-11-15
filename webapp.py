14b343b6153dfc0aaa44f1f96c1067c504d183f5
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-11-15 13:28:40 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, request, Markup, flash
-import os, json
+import os, json, random
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ with open('energy.json') as energy_data:
 @app.route("/")
 def render_main():
     return render_template('Home.html')
+
+def us_importEngery():
+       imprts = energy['u\'imports']
+       randKey = random.choice(list(imprts.keys()))
+       
+       return randKey + ": "
+       
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
