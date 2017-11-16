425ccf55d4fd06292f8ce10a9ff874a53f603666
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-11-16 09:52:43 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,6 +10,26 @@ with open('energy.json') as energy_data:
 def render_main():
     return render_template('Home.html')
 
+@app.route("/importEnergy")
+def render_import():
+       us_imp = request.args["year"]
+       return rendr_template('importEnergy.html', year = get_year_options(us_imp))
+
+def get_year_options(default = None):
+    
+    options = ""
+    state = ""
+    for c in energy:
+        if not c["year"] == state:
+            if c["year"] == default:  
+              options += Markup("<option selected value=\"" + c["year"] + "\">" + c["year"] + "</option>")
+            else:
+              options += Markup("<option value=\"" + c["year"] + "\">" + c["year"] + "</option>")
+            state = c["year"]
+            #print(options)
+    
+    return options
+
 def us_importEngery(year):
        imprts = energy[1949-year]["imports"]
        randKey = random.choice(list(imprts.keys()))
