eb7cc2242011f1227983effff2b965c9fff72181
TheNumberGame <skyler.chandos@yahoo.com>
2017-11-17 09:55:12 -0800

    hi

--- webapp.py
+++ webapp.py
@@ -10,24 +10,29 @@ with open('energy.json') as energy_data:
 def render_main():
     return render_template('Home.html')
 
-@app.route("/importEnergy")
-def render_imports():
+@app.route("/UsImprt1")
+def render_usprts1():
+       return render_template('importEnergy.html', year = get_year_options())
+       
+@app.route("/UsImprt2")
+def render_usprts2():
        us_imp = request.args["year"]
-       return rendr_template('importEnergy.html')
+       print(us_imp)
+       return render_template('importEnergy.html', year = get_year_options(us_imp))
 
-#def get_year_options(default = None):    
-#    options = ""
-#    state = ""
-#    for c in energy:
-#        if not c["year"] == state:
-#            if c["year"] == default:  
-              #options += Markup("<option selected value=\"" + c["year"] + "\">" + c["year"] + "</option>")
-#            else:
-              #options += Markup("<option value=\"" + c["year"] + "\">" + c["year"] + "</option>")
-#            state = c["year"]
-            #print(options)
+def get_year_options(default = None):    
+    options = ""
+    state = 0
+    for c in energy:
+        if not c["year"] == state:
+            if c["year"] == default:  
+              options += Markup("<option selected value=\"" + str(c["year"]) + "\">" + str(c["year"]) + "</option>")
+              print('true')
+            else:
+              options += Markup("<option value=\"" + str(c["year"]) + "\">" + str(c["year"]) + "</option>")
+            state = c["year"]
     
-    #return options
+    return options
 
 def us_importEngery(year):
        imprts = energy[1949-year]["imports"]
