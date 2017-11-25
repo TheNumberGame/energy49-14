5384f5d71a3236be680f8c753127648603809947
TheNumberGame <skyler.chandos@yahoo.com>
2017-11-25 11:46:21 -0800

    Going back

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-str(from flask import Flask, url_for, render_template, request, Markup, flash
+from flask import Flask, url_for, render_template, request, Markup, flash
 import os, json, random
 
 app = Flask(__name__)
@@ -13,33 +13,33 @@ def render_main():
 @app.route("/UsImprt1")
 def render_usprts1():
        return render_template('importEnergy.html', year = get_year_options())
-       
+
 @app.route("/UsImprt2")
 def render_usprts2():
        us_imp = request.args["year"]
        print(us_imp)
        return render_template('importEnergy.html', year = get_year_options(us_imp))
 
-def get_year_options(default = None):    
+def get_year_options(default = None):
     options = ""
     state = ""
     for c in energy:
         if not str(c["year"]) == state:
-            if str(c["year"]) == default:  
+            if str(c["year"]) == default:
               options += Markup("<option selected value=\"" + default + "\">" + default + "</option>")
               print('true')
             else:
               options += Markup("<option value=\"" + str(c["year"]) + "\">" + str(c["year"]) + "</option>")
             state = str(c["year"])
-    
+
     return options
 
 def us_importEngery(year):
        imprts = energy[1949-year]["imports"]
        randKey = random.choice(list(imprts.keys()))
-       
-       return randKey + ": " + imprts[randKey] + " Quadrillion BTUs" 
-       
+
+       return randKey + ": " + imprts[randKey] + " Quadrillion BTUs"
+
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
