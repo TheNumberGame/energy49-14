d64895f2de1a628c0efd25d057bd6f03ea713f0b
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-01-09 09:53:00 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -70,7 +70,7 @@ def us_prCnEnergy(year):
 	except:
 	    imprts2 = "None"
 	
-	return "US Production: " +  randKey + ": " + str(imprts[randKey]) + " Quadrillion BTUs; " + " US Consumtion " + randKey + " " + str(imprts2)
+	return "US Production: " +  randKey + ": " + str(imprts[randKey]) + " Quadrillion BTUs; " + " US Consumtion " + randKey + " " + str(imprts2) + " Quadrillion BTUs."
 
 
 if __name__=="__main__":
